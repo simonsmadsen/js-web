9b3e9978ade0d618115e4a1e3828d0265b0abead
Grigoriy Grudinin <grgr14@mail.ru>
2017-10-06 12:51:15 +0300

    new test route added

--- test/routing/index.test.js
+++ test/routing/index.test.js
@@ -1,41 +1,63 @@
+import request from 'supertest'
+
 import { route, htmlRoute, postRoute, app } from './../../src/routing'
 
-const request = require('supertest')
+// process.on('unhandledRejection', (reason, p) => {
+//   console.log(p)
+// })
 
-test('get route (route)', async () => {
-  const jsonResponse = { simon: 'my name2' }
-  route('/test', () => (jsonResponse))
+describe('routing', () => {
+  let server
 
-  const result = await request(app).get('/test')
+  beforeEach(() => {
+    server = app.listen()
+  })
 
-  expect(result.status).toBe(200)
-  expect(result.body).toEqual(jsonResponse)
-})
+  afterEach((done) => {
+    server.close()
+    done()
+  })
 
-test('post route (postRoute)', async () => {
-  const jsonResponse = { simon: 'my name2' }
-  postRoute('/test-post', () => (jsonResponse))
+  it('get route (route)', async () => {
+    const jsonResponse = { simon: 'my name2' }
+    await route('/test', () => (jsonResponse))
 
-  const result = await request(app).post('/test-post')
+    const result = await request(server).get('/test')
+    expect(result.status).toBe(200)
+    expect(result.body).toEqual(jsonResponse)
+  })
 
-  expect(result.status).toBe(200)
-  expect(result.body).toEqual(jsonResponse)
-})
+  it('get route (route) empty data', async () => {
+    await route('/test-empty')
 
-test('html route (htmlRoute)', async () => {
-  const htmlFile = 'test/routing/html-test-file.html'
-  htmlRoute('/test-html', htmlFile, async () => ({name: 'Simon'}))
+    const result = await request(server).get('/test-empty')
+    expect(result.status).toBe(200)
+    expect(result.body).toEqual({})
+  })
 
-  const result = await request(app).get('/test-html')
-  expect(result.status).toBe(200)
-  expect(result.text.indexOf('<h1>Hi Simon</h1>') > -1).toBeTruthy()
-})
+  it('post route (postRoute)', async () => {
+    const jsonResponse = { simon: 'my name2' }
+    await postRoute('/test-post', () => (jsonResponse))
 
-test('html route2 (htmlRoute)', async () => {
-  const htmlFile = 'test/routing/html-test-file.html'
-  htmlRoute('/test-html2', htmlFile, async () => ({name: 'Simon2'}))
+    const result = await request(server).post('/test-post').send(jsonResponse)
+    expect(result.status).toBe(200)
+    expect(result.body).toEqual(jsonResponse)
+  })
 
-  const result = await request(app).get('/test-html2')
-  expect(result.status).toBe(200)
-  expect(result.text.indexOf('<h1>Hi Simon</h1>') === -1).toBeTruthy()
+  it('html route (htmlRoute)', async () => {
+    const htmlFile = 'test/routing/html-test-file.html'
+    await htmlRoute('/test-html', htmlFile, async () => ({ name: 'Simon' }))
+
+    const result = await request(server).get('/test-html')
+    expect(result.status).toBe(200)
+    expect(result.text.includes('<h1>Hi Simon</h1>')).toBeTruthy()
+  })
+
+  it('html route file not exists', async () => {
+    const htmlFile = 'test/routing/fake-file.html'
+    await htmlRoute('/test-html3', htmlFile, async () => ({ name: 'Simon' }))
+
+    const result = await request(server).get('/test-html3')
+    expect(result.status).toBe(404)
+  })
 })
